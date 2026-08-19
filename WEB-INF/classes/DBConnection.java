c7441fccd0a80575d3ec957e4eed8b7e3278ee4c
shanmukhiuppalapati1206 <usanjupriya@gmail.com>
2026-08-19 22:21:08 +0530

    Update DBConnection.java

--- WEB-INF/classes/DBConnection.java
+++ WEB-INF/classes/DBConnection.java
@@ -1,14 +1,13 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DBConnection {
-
     public static Connection getConnection() throws Exception {
-
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-        String url = System.getenv("DB_URL");
-        String user = System.getenv("DB_USER");
-        String password = System.getenv("DB_PASSWORD");
+        String url = "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/pharmamate?useSSL=true&sslMode=PREFERRED&allowPublicKeyRetrieval=true";
+        String user = "YOUR_TIDB_USERNAME_HERE";
+        String password = "YOUR_TIDB_PASSWORD_HERE";
 
         return DriverManager.getConnection(url, user, password);
     }
