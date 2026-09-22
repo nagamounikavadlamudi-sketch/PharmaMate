0901934fe0ba143ec0fe0e0823dad13739a35cd7
nagamounikavadlamudi-sketch <nagamounikavadlamudi@gmail.com>
2026-09-22 21:47:03 +0530

    updated expiry

--- WEB-INF/classes/ExpiryNotification.java
+++ WEB-INF/classes/ExpiryNotification.java
@@ -184,10 +184,11 @@ public class ExpiryNotification extends HttpServlet {
 
         Properties properties = new Properties();
         properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "587");
         properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.starttls.required", "true");
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
